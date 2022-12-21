04b28210c185244f1c52f13637d2dec8152b6ed7
spartanDavid22 <90160219+spartanDavid22@users.noreply.github.com>
2022-12-20 23:42:21 -0500

    Added the about me section

--- js/mobile/navigation.js
+++ js/mobile/navigation.js
@@ -5,9 +5,18 @@ iconToggle.addEventListener("click",(event)=>{
     
     if(navLinks.style.display == "none"){
         navLinks.style.display = "block";
+        iconToggle.innerHTML = "<i class=\"fa-solid fa-xmark\"></i>"
     }
     else{
         navLinks.style.display = "none";
+        iconToggle.innerHTML = "<i class=\"fa-solid fa-bars\"></i>"
     }
-    
+
+})
+
+document.querySelectorAll("#about, #contact, #projects, #skills").forEach(tag =>{
+    tag.addEventListener("click", event =>{
+
+        document.getElementById("navLinks").style.display = "none";
+    })
 })
